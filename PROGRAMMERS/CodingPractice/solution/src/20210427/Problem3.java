4262203b0885dc341bfccaa916cdead6dd7321e1
dahun3013 <82797327+dahun3013@users.noreply.github.com>
2021-04-27 21:35:17 +0900

    Create Problem3.java

---
+++ PROGRAMMERS/CodingPractice/solution/src/20210427/Problem3.java
@@ -0,0 +1,31 @@
+// 큰 수 만들기
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+
+        int size = number.length();
+
+        int start = 0;
+        int cnt = size - k;
+        int end = size - cnt;
+
+        while (true) {
+            if (answer.length() == size - k) break;
+            int idx = start;
+            int bigger = -1;
+            for (int i = start; i <= end; i++) {
+                if (i >= number.length()) break;
+                    int cur = number.charAt(i) - '0';
+                if (bigger < cur) {
+                    bigger = cur;
+                    idx = i;
+                }
+            }
+            answer.append(bigger);
+            start = idx + 1;
+            end = size - --cnt;
+        }
+
+        return answer.toString();
+    }
+}
