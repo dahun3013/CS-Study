a62101a150fed31b6cf1900e179c69403716b936
dahun3013 <82797327+dahun3013@users.noreply.github.com>
2021-05-01 16:45:34 +0900

    Create Problem2.java

---
+++ PROGRAMMERS/CodingPractice/solution/src/Problem20210501/Problem2.java
@@ -0,0 +1,20 @@
+//3진법 뒤집기
+class Solution {
+    public int solution(int n) {
+        String str = "";
+        int temp = 0;
+        int answer = 0;
+        while(n / 3 != 0){
+            str+=n%3;
+            n/=3;
+        }
+        str+=String.valueOf(n);
+        
+        for(int i = str.length()-1;i>=0;i--){          
+            answer+=((str.charAt(i) - '0') * (int)Math.pow(3,temp));         
+            temp++;
+        }
+        
+        return answer;
+    }
+}
