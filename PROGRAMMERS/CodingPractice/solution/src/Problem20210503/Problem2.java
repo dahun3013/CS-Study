97708622f475968ab0fb35d9a9866455e2afb302
dahun3013 <82797327+dahun3013@users.noreply.github.com>
2021-05-03 19:33:12 +0900

    Create Problem2.java

---
+++ PROGRAMMERS/CodingPractice/solution/src/Problem20210503/Problem2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+//이진 변환 반복하기
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = {};
+        int zero=0, cnt=0;
+        while(!s.equals("1")){
+            cnt++;
+            String removeZero=s.replace("0","");
+            zero+=s.length()-removeZero.length();
+            s=Integer.toBinaryString(removeZero.length());
+        }
+        
+        answer=new int[2];
+        answer[0]=cnt;
+        answer[1]=zero;
+        return answer;
+    }
+}
